381f058de1d8c4b34af8237fd24eeef89469878a
mridulaus <mradulashety96@gmail.com>
2021-07-16 22:40:47 +0530

    initial daostore

---
+++ src/com/xworkz/storedata/dao/RiverDAO.java
@@ -0,0 +1,8 @@
+package com.xworkz.storedata.dao;
+import com.xworkz.storedata.dto.*;
+public interface RiverDAO {
+	
+	
+	boolean save(RiverDTO dto);
+	
+}
---
+++ src/com/xworkz/storedata/dao/RiverDAOImpl.java
@@ -0,0 +1,12 @@
+package com.xworkz.storedata.dao;
+
+import com.xworkz.storedata.dto.*;
+
+public class RiverDAOImpl implements RiverDAO {
+
+	@Override
+	public boolean save(RiverDTO dto) {
+		return false;
+
+	}
+}
